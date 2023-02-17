dce09e33b5382534e1b6c804049b6dd98de7adcf
eivind <eivind.askestad@nav.no>
2023-02-17 14:20:33 +0100

    Omsorgsarbeids.omsorgsAr is to avoid anything other than numbers

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/OmsorgsArbeid.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/OmsorgsArbeid.kt
@@ -3,11 +3,11 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages
 import java.time.YearMonth
 
 
-data class OmsorgsArbeidKey(val omsorgsYter: String, val omsorgsAr: String, val omsorgsType: Omsorgstype)
+data class OmsorgsArbeidKey(val omsorgsYter: String, val omsorgsAr: Int, val omsorgsType: Omsorgstype)
 
 data class OmsorgsarbeidsSnapshot(
     val omsorgsYter: Person,
-    val omsorgsAr: String,
+    val omsorgsAr: Int,
     val omsorgstype: Omsorgstype,
     val kjoreHash: String,
     val kilde: Kilde,
