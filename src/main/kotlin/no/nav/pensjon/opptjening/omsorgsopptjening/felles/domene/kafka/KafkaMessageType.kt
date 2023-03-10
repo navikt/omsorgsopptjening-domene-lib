25851aef77251c990cae5ede5365e72fa133779e
Mo Amini <mo.amini@nav.no>
2023-03-10 14:02:23 +0100

    Legger til ny messagetype for RETRY
    Co-authored-by: eivind.askestad@nav.no

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/KafkaMessageType.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/KafkaMessageType.kt
@@ -1,4 +1,4 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka
 
-enum class KafkaMessageType { OMSORGSOPPTJENING, OMSORGSARBEID }
+enum class KafkaMessageType { OMSORGSOPPTJENING, OMSORGSARBEID, RETRY }
 
