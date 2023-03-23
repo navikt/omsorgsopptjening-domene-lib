da2f82058506785d412eafcdc0a1cf1683f7c85f
eivind <eivind.askestad@nav.no>
2023-03-23 11:32:46 +0100

    Renamed Omsorgstype to Omsorgsarbeidstype and Kilde to OmsorgsarbeidsKilde

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/OmsorgsArbeid.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/OmsorgsArbeid.kt
@@ -3,24 +3,24 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages
 import java.time.YearMonth
 
 
-data class OmsorgsArbeidKey(val omsorgsyter: String, val omsorgsAr: Int, val omsorgsType: Omsorgstype)
+data class OmsorgsArbeidKey(val omsorgsyter: String, val omsorgsAr: Int, val omsorgsType: Omsorgsarbeidstype)
 
 data class OmsorgsarbeidsSnapshot(
     val omsorgsyter: Person,
     val omsorgsAr: Int,
-    val omsorgstype: Omsorgstype,
+    val omsorgstype: Omsorgsarbeidstype,
     val kjoreHash: String,
-    val kilde: Kilde,
+    val kilde: OmsorgsarbeidsKilde,
     val omsorgsArbeidSaker: List<OmsorgsArbeidSak>
 )
 
-enum class Omsorgstype {
+enum class Omsorgsarbeidstype {
     BARNETRYGD,
     HJELPESTØNAD_SATS_3,
     HJELPESTØNAD_SATS_4
 }
 
-enum class Kilde {
+enum class OmsorgsarbeidsKilde {
     BA, IT
 }
 
