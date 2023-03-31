e55b81b4989636bd42e868e1e1a3b2ef1eb65af2
Mo Amini <mo.amini@nav.no>
2023-03-31 08:59:54 +0200

    Refactoring av Person equals metode

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/Person.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/Person.kt
@@ -1,15 +1,7 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages
 
 class Person(val fnr: String, var fodselsAr: String? = null) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Person) return false
-        if(this.fnr === other.fnr) return true
-        return false
-    }
-
-    override fun hashCode(): Int {
-        return fnr.hashCode()
-    }
+    override fun equals(other: Any?) = this === other || (other is Person && this.fnr == other.fnr)
+    override fun hashCode() = fnr.hashCode()
 }
 
