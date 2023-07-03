038cbe924668151fc06e08f2c30178184a38ef87
Mo Amini <mo.amini@nav.no>
2023-07-03 09:42:12 +0200

    Legger til namespace til topicnavn

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/Topics.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/Topics.kt
@@ -4,7 +4,7 @@ object Topics {
     const val BARNETRYGDMOTTAKER= "barnetrygdmottaker"
 
     object Omsorgsopptjening {
-        const val NAME = "omsorgsopptjening"
+        const val NAME = "pensjonopptjening.omsorgsopptjening"
         data class Key(val ident: String)
     }
 
