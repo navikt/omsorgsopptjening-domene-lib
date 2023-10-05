a9a23a3101b14e512048e94241507578b8aa84d8
Jacob Meidell <jacob.meidell@nav.no>
2023-10-05 08:22:51 +0200

    Add new type for bt

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/domene/Omsorgstype.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/kafka/messages/domene/Omsorgstype.kt
@@ -3,6 +3,7 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages
 enum class Omsorgstype {
     DELT_BARNETRYGD,
     FULL_BARNETRYGD,
+    USIKKER_BARNETRYGD, //ikke nok data til å avgjøre om det er delt eller full, kan være tilfellet på gamle saker fra Infotrygd
     HJELPESTØNAD_FORHØYET_SATS_3,
     HJELPESTØNAD_FORHØYET_SATS_4;
 }
