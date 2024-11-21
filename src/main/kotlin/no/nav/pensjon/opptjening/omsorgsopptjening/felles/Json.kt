52a761b85a28893e81887477392446efbd71e636
Jan Kroken <jan.kroken@nav.no>
2024-11-21 23:00:50 +0100

    fiks på serialisering

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/Json.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/Json.kt
@@ -3,10 +3,14 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.felles
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
-val mapper : ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+val mapper: ObjectMapper = jacksonObjectMapper()
+    .registerModule(KotlinModule.Builder().build())
+    .registerModule(JavaTimeModule())
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
 fun Any.mapToJson(): String = mapper.writeValueAsString(this)
 
@@ -15,6 +19,7 @@ fun <T> String.mapToClass(valueType: Class<T>): T = mapper.readValue(this, value
 fun serialize(value: Any): String {
     return mapper.writeValueAsString(value)
 }
+
 inline fun <reified T> deserialize(value: String): T {
     return mapper.readValue(value)
 }
--- src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/JsonTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/felles/domene/JsonTest.kt
@@ -1,9 +1,8 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene
 
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.deserializeList
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.mapToClass
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.mapToJson
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.serializeList
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.*
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages.domene.Feilinformasjon
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages.domene.IdentRolle
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -27,11 +26,32 @@ class JsonTest {
     fun `kan serialisere og deserialisere en liste av data-klasse`() {
         val person1 = Person("Ola Nordmann", 42)
         val person2 = Person("Kari Nordmann", 42)
-        val inputList = listOf(person1,person2)
+        val inputList = listOf(person1, person2)
         val lestePersoner = inputList.serializeList().deserializeList<Person>()
         assertThat(lestePersoner).containsExactlyElementsOf(inputList)
     }
 
+    @Test
+    fun `kan serialsere og deserialse en liste av feilinformasjon`() {
+        val ugyldigIdent = Feilinformasjon.UgyldigIdent(
+            message = "m",
+            exceptionType = "et1",
+            exceptionMessage = "em",
+            ident = "ident",
+            identRolle = IdentRolle.BARNETRYGDMOTTAKER,
+        )
+        val overlappendeBarnetrygdperioder = Feilinformasjon.OverlappendeBarnetrygdperioder(
+            message = "obpm",
+            exceptionType = "et",
+            exceptionMessage = "em",
+            omsorgsmottaker = "om",
+        )
+        val list = listOf(ugyldigIdent, overlappendeBarnetrygdperioder)
+        val serialized = list.serializeList()
+        val newList = serialized.deserializeList<Feilinformasjon>()
+        assertThat(newList).isEqualTo(list)
+    }
 
-    data class Person(val navn:String, val alder:Int)
+
+    data class Person(val navn: String, val alder: Int)
 }
